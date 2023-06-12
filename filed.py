837d4cd07c0eabb07faa49f34f9619ec3803d1cc
d uqc <57027299+duqc@users.noreply.github.com>
2023-06-11 22:52:24 -0600

    Update filed.py

--- filed.py
+++ filed.py
@@ -21,7 +21,7 @@ class wall():
             left = abs(point[0] - self.pos2[0])
             down = abs(point[1] - self.pos1[1])
             up = abs(point[1] - self.pos2[1])
-
+            
             thething = 1
             smallest = right
             if smallest > left:
@@ -35,6 +35,18 @@ class wall():
             return (True, thething)
         else:
             return (False, 0)
+        
+class winnermode(wall):
+    def __init__(self, pos1, pos2):
+        super().__init__(pos1,pos2)
+    def collision(self, point):
+        if point[0] >= self.pos1[0] and point[0] <= self.pos2[0] and point[1] >= self.pos1[1] and point[1] <= self.pos2[1]:
+            return(True, 5)
+        else: 
+            return(False,0)
+    def draw(self):
+        pygame.draw.rect(screen,GREEN,(self.pos1[0],self.pos1[1],self.sizex,self.sizey))
+        
 
 class projectile():
     def __init__(self,pos,angle):
@@ -43,6 +55,7 @@ class projectile():
         self.ticker = 0
         self.timer = 500
     def update(self,distance, walls):
+        
         if self.ticker != 0:
             self.ticker -= 1
         x1 = distance * math.sin(self.angle)
@@ -50,23 +63,40 @@ class projectile():
         if self.ticker <= 0:
             collided = False
             for obj in walls:
-                if obj.collision([self.pos[0]+x1,self.pos[1]+y1])[0]:
+                this = obj.collision([self.pos[0]+x1,self.pos[1]+y1])
+                if this[0]:
+                    if this[1] == 5:
+                        return "lol"
                     collided = True
                     collisionpos = [self.pos[0]+x1,self.pos[1]+y1]
                     collidedObj = obj
                     break
             if collided:
-                drawline(self.pos, collisionpos, collidedObj)
+                bruh = drawline(self.pos, collisionpos, collidedObj)
+                print(bruh[0])
+                match bruh[0]:
+                    case 1:
+                        self.angle = -self.angle
+                        self.pos = bruh[1]
+                    case 2:
+                        self.angle = -self.angle
+                        self.pos = bruh[1]
+                    case 3:
+                        normal = math.pi
+                        self.angle = normal-self.angle
+                        self.pos = bruh[1]
+                    case 4:
+                        normal = math.pi
+                        self.angle = normal-self.angle
+                        self.pos = bruh[1]
+
             else:
                 self.pos = [self.pos[0]+x1,self.pos[1]+y1]
 
             self.ticker = 30
         pygame.draw.circle(screen,(0,0,255), self.pos, 2)
         self.timer -= 1
-        if self.timer == 0:
-            return True
-        else:
-            return False
+        return self.timer == 0 
 
 
 class player():
@@ -116,15 +146,11 @@ def drawline(pos1,pos2,obj):
 
     samplesize = 20
 
-    for n in range(samplesize):
+    for n in range(samplesize*2):
         if obj.collision(calculation((n*2)+1, pos1, xdeviance,ydeviance,samplesize))[0]:
-
             sidehit = obj.collision(calculation((n*2)+1, pos1, xdeviance,ydeviance,samplesize))[1]
-
-            print(sidehit)
-
             incursionpos = calculation((n*2)+1, pos1, xdeviance,ydeviance,samplesize)
-            break
+            return sidehit, incursionpos
 
 
 
@@ -138,7 +164,7 @@ testing = False
 
 pygame.init()
 size = (1000,700)
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Game")
 done = False
 clock = pygame.time.Clock()
@@ -154,14 +180,18 @@ bullets = []
 
 
 walls = []
-walls.append(wall((0,0),(size[0],10)))
-walls.append(wall((size[0]-20,0),(size[0],size[1]+60)))
-walls.append(wall((0,0),(10,size[1]+60)))
-walls.append(wall((0,size[1]-10),(size[0],size[1])))
+walls.append(wall((-100,-100),(size[0],10)))
+walls.append(wall((size[0]-20,-100),(size[0]+100,size[1]+60)))
+walls.append(wall((-100,-100),(10,size[1]+60)))
+walls.append(wall((-100,size[1]-10),(size[0]+100,size[1]+100)))
+
 
 
 
 walls.append(wall((300,400),(600,420)))
+walls.append(winnermode((300,450),(350,500)))
+
+
 
 # -------- Main Program Loop -------------------------------------------------------------------------------------------------------------------
 while not done:
@@ -169,20 +199,10 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == pygame.VIDEORESIZE:
-            surface = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
-            size = (event.w,event.h)
-            print("hallal")
-            walls[0] = wall((0,0),(size[0],10))
-            walls[1] = wall((size[0]-20,0),(size[0],size[1]+60))
-            walls[2] = wall((0,0),(10,size[1]+60))
-            walls[3] = wall((0,size[1]-10),(size[0]+60,size[1]+60))
 
     screen.fill(BLACK)
 
     playerobj.update(walls)
-
-
 
     if keyboard.is_pressed("right"):
         angle -= 1
@@ -215,8 +235,15 @@ while not done:
         pygame.draw.circle(screen,(255,255,255),shootpos,2)
 
     for obj in bullets:
-            if obj.update(50,walls):
+            winner = obj.update(50,walls)
+            if winner == "lol":
+                walls = walls[:4]
                 bullets.remove(obj)
+                playerobj = player([500,300])
+                angle = 0
+            elif winner:
+                bullets.remove(obj)
+
 
     pygame.display.flip()
     # --- Limit to 60 frames per second
